e8b383498f6957b4a47f4627c2db6ead2b3b3618
Chu Minh <chuminh712@gmail.com>
2019-03-16 22:35:15 +0700

    Finished login feature

---
+++ Construction/SourceCode/BookStorage/BookStorage/Models/GoodsIssueInfo.cs
@@ -0,0 +1,28 @@
+namespace BookStorage.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("GoodsIssueInfo")]
+    public partial class GoodsIssueInfo
+    {
+        public int ID { get; set; }
+
+        public int? BookID { get; set; }
+
+        public int? GoodsIssueID { get; set; }
+
+        public int? ReceiptQuantity { get; set; }
+
+        public int? RealQuantity { get; set; }
+
+        public decimal? TotalPrice { get; set; }
+
+        public virtual Book Book { get; set; }
+
+        public virtual GoodsIssue GoodsIssue { get; set; }
+    }
+}
---
+++ Construction/SourceCode/BookStorage/BookStorage/Models/Unit.cs
@@ -0,0 +1,26 @@
+namespace BookStorage.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("Unit")]
+    public partial class Unit
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Unit()
+        {
+            Books = new HashSet<Book>();
+        }
+
+        public int ID { get; set; }
+
+        [StringLength(500)]
+        public string Name { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Book> Books { get; set; }
+    }
+}
