db6398c86c14645b64d1fadcf222afcbc9057ec0
quangdd1298 <48044711+quangdd1298@users.noreply.github.com>
2019-04-16 17:03:02 +0700

    Revert "QuangDD edit CRUD Book"
    This reverts commit bc2fabb3589c36d71b221cd4c3a8f5aa20f15eb9.

--- Construction/SourceCode/BookStorage/BookStorage/Models/BookCategory.cs
+++ Construction/SourceCode/BookStorage/BookStorage/Models/BookCategory.cs
@@ -5,27 +5,13 @@ namespace BookStorage.Models
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Data.Entity.Spatial;
-    using System.Linq;
 
     [Table("BookCategory")]
     public partial class BookCategory
     {
-        BookStorageDbContext db = null;
-
-        public BookCategory()
-        {
-            db = new BookStorageDbContext();
-        }
-
-        public List<BookCategory> ListAll()
-        {
-            return db.BookCategories.ToList();
-        }
-
         public int ID { get; set; }
 
         [StringLength(500)]
         public string Name { get; set; }
-
     }
 }
