f4ceebe66fc70594a5645daec8d57c70a5de9348
KhaiNHb <20168712@sie.edu.vn>
2019-04-25 14:40:49 +0700

    Update Goods-Issue-Info and Goods-Issue

--- Construction/SourceCode/BookStorage/BookStorage/Models/GoodsIssue.cs
+++ Construction/SourceCode/BookStorage/BookStorage/Models/GoodsIssue.cs
@@ -1,18 +1,19 @@
-namespace BookStorage.Models
+﻿namespace BookStorage.Models
 {
+    using System;
+    using PagedList;
     using System;
     using System.Collections.Generic;
     using System.ComponentModel.DataAnnotations;
     using System.ComponentModel.DataAnnotations.Schema;
-    using PagedList;
     using System.Data.Entity.Spatial;
     using System.Linq;
     using System.Web.Mvc;
+
     [Table("GoodsIssue")]
     public partial class GoodsIssue
     {
         BookStorageDbContext db = null;
-
 
         public GoodsIssue()
         {
@@ -21,6 +22,18 @@ namespace BookStorage.Models
 
         public int Insert(GoodsIssue entity)
         {
+            if (entity.GoodsIssueInfo != null)
+            {
+                entity.GoodsIssueInfo.RemoveAll(x => x.BookID == null);
+                foreach (var item in entity.GoodsIssueInfo)
+                {
+                    var book = db.Books.Find(item.BookID);
+                    if (book != null)
+                    {
+                        book.Quantity += item.RealQuantity;
+                    }
+                }
+            }
             db.GoodsIssues.Add(entity);
             db.SaveChanges();
             return entity.ID;
@@ -63,7 +76,14 @@ namespace BookStorage.Models
             try
             {
                 var goodsIssue = db.GoodsIssues.Find(id);
+                var goodsIssueInfoes = db.GoodsIssueInfoes.Where(x => x.GoodsIssueID == id);
+                foreach (var item in goodsIssueInfoes)
+                {
+                    var book = db.Books.Find(item.BookID);
+                    book.Quantity -= item.RealQuantity;
+                }
                 db.GoodsIssues.Remove(goodsIssue);
+                db.GoodsIssueInfoes.RemoveRange(goodsIssueInfoes);
                 db.SaveChanges();
                 return true;
             }
@@ -78,31 +98,51 @@ namespace BookStorage.Models
             return db.GoodsIssues.Find(id);
         }
 
-        
+
 
         public int ID { get; set; }
-        public virtual List<GoodsIssue> GoodsIssues { get; set; }
+        public virtual List<GoodsIssueInfo> GoodsIssueInfo { get; set; }
+
 
         [StringLength(500)]
+        [Display(Name = "Người nhận")]
         public string ReciverName { get; set; }
 
         [Column(TypeName = "date")]
+        [Display(Name = "Ngày tạo")]
         public DateTime? CreatedDate { get; set; }
 
+        [Display(Name = "Khách hàng")]
         public int? CustomerID { get; set; }
+        public virtual Customer Customer { get; set; }
 
         [StringLength(50)]
+        [Display(Name = "Nợ")]
         public string Debit { get; set; }
 
         [StringLength(50)]
+        [Display(Name = "Có")]
         public string Credit { get; set; }
 
         [StringLength(200)]
+        [Display(Name = "Mã phiếu nhập")]
         public string Code { get; set; }
 
         [StringLength(800)]
+        [Display(Name = "Lý do xuất")]
         public string OutputReason { get; set; }
 
+        [Display(Name = "Tổng số tiền")]
+        [DisplayFormat(DataFormatString = "#.##0")]
+        public decimal? TotalPrice { get; set; }
+
+
+        [Display(Name = "Trạng thái")]
         public bool Status { get; set; }
+
+        //public int? BookID { get; internal set; }
+        //public int? GoodsIssueID { get; internal set; }
+        //public int? ReceiptQuantity { get; internal set; }
+        //public int? RealQuantity { get; internal set; }
     }
 }
