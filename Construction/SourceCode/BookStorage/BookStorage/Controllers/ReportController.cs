77a20daa402a89acbe80ade211303b05017a7ba7
Chu Minh <chuminh712@gmail.com>
2019-05-04 03:31:19 +0700

    View Inventory Report feature

---
+++ Construction/SourceCode/BookStorage/BookStorage/Controllers/ReportController.cs
@@ -0,0 +1,21 @@
+﻿using BookStorage.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookStorage.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Inventory
+        public ActionResult InventoryReport(string searchString)
+        {
+            var dao = new InventoryModel();
+            var model = dao.GetInventoryInfo(searchString);
+            ViewBag.SearchString = searchString;
+            return View(model);
+        }
+    }
+}
---
+++ Construction/SourceCode/BookStorage/BookStorage/ViewModels/InventoryModel.cs
@@ -0,0 +1,57 @@
+﻿using BookStorage.Models;
+using PagedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookStorage.ViewModels
+{
+    public class InventoryModel
+    {
+        BookStorageDbContext db = null;
+
+        public InventoryModel()
+        {
+            db = new BookStorageDbContext();
+        }
+
+        public string BookName { get; set; }
+        public string Image { get; set; }
+        public string Unit { get; set; }
+        public string Code { get; set; }
+        public string Publisher { get; set; }
+        public int? Quantity { get; set; }
+        public DateTime? CreateDate { get; set; }
+        public int? GoodsReceiptQuantity { get; set; }
+        public int? GoodsIssueQuantity { get; set; }
+
+        public IEnumerable<InventoryModel> GetInventoryInfo(string searchString)
+        {
+            var model = (from a in db.Books
+                         join b in db.GoodsReceiptInfoes
+                         on a.ID equals b.BookID
+                         into inventory
+                         join c in db.GoodsIssueInfoes
+                         on a.ID equals c.BookID
+                         into inventory2
+                         select new InventoryModel()
+                         {
+                             BookName = a.Name,
+                             Image = a.Image,
+                             Code = a.Code,
+                             Unit = a.Unit.Name,
+                             Publisher = a.Publisher,
+                             Quantity = a.Quantity,
+                             CreateDate = a.CreatedDate,
+                             GoodsReceiptQuantity = inventory.Sum(x => x.RealQuantity),
+                             GoodsIssueQuantity = inventory2.Sum(c => c.RealQuantity)
+                         });
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.Code.Contains(searchString) || x.BookName.Contains(searchString) || x.Publisher.Contains(searchString));
+            }
+            return model.OrderByDescending(x => x.CreateDate).ToPagedList(1, 10);
+        }
+    }
+}
